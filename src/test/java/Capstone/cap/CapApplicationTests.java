c4ecd42b7f87405605cc96e4169e5b613fbf24f6
Ankit <agarwalankit918@gmail.com>
2022-09-06 15:19:46 +0530

    Add to repo

---
+++ src/test/java/Capstone/cap/CapApplicationTests.java
@@ -0,0 +1,13 @@
+package Capstone.cap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
